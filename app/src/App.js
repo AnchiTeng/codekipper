35f2d0a4014663fc6df89b812ae22ad807191fbf
Ashley Pean <pean.ashley@gmail.com>
2021-07-31 17:44:21 -0400

    starter template for homepage

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Container, Button, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
 
-function App() {
+const useStyles = makeStyles({
+  mainContainer: {
+    width: '100%', 
+    margin: 0, 
+    padding: 0,
+    display: 'grid', 
+    gridTemplateAreas: 
+    `
+    "search search search search"
+    "folder code code code"
+    "folder code code code"
+    "folder code code code"
+    "folder code code code"
+    `
+  },
+  searchContainer: {
+    width: '100%',
+    height: '100%',
+  }, 
+  folderContainer: {
+
+  }, 
+  codeContainer: {
+  
+  },
+});
+
+const App = () => {
+  const classes = useStyles();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [folderModalOpen, setFolderModalOpen] = useState(false);
+  const [codeModalOpen, setCodeModalOpen] = useState(false);
+  const [activeFolder, setActiveFolder] = useState(null);
+  const [folders, setFolders] = useState([]);
+  const [codeSnippets, setCodeSnippets] = useState([]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.mainContainer}>
+      <Container className={classes.searchContainer}>
+        <Button>
+          <AddIcon />
+        </Button>
+        <TextField
+          id="search-query"
+          value={searchQuery}
+          label="Search"
+          onChange={e => setSearchQuery(e.target.value)}
+        />
+      </Container>
+    </Container>
   );
 }
 
